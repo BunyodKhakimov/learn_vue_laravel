7ae1e3e58bbc7fdbf69956c01888cc8eeb10e21b
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-06-15 13:48:10 +0500

    install vue-router, add router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import router from "./router";
 
 require('./bootstrap');
 
@@ -7,5 +8,7 @@ const app = new Vue({
 
     components: {
         //
-    }
+    },
+
+    router
 })
