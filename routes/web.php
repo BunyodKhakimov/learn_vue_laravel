ebf0a7026adfe4a4e86bd2cf215a84c276a0fdf8
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-06-15 16:41:24 +0500

    fix php laravel router for spa

--- routes/web.php
+++ routes/web.php
@@ -21,5 +21,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/{page}', 'IndexController');
+Route::get('/{page}', 'IndexController')->where('page', '.*');
 
