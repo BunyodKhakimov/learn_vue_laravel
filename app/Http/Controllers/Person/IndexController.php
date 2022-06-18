21297f38875e2c690a999e768ea57bb17d7fb362
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-06-18 20:18:22 +0500

    add person resource & collection

--- app/Http/Controllers/Person/IndexController.php
+++ app/Http/Controllers/Person/IndexController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Person;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PersonCollection;
 use App\Models\Person;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        return Person::all();
+        return new PersonCollection(Person::all());
     }
 }
